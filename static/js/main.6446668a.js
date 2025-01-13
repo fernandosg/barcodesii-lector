532e256ea49a875cfda7aa5d71b58a08cf485323
fernandosg <fsg_tmp@hotmail.com>
2025-01-13 15:39:53 -0600

    Updates

--- static/js/main.6446668a.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e=sessionStorage.getItem("scannedCode");null===e?document.getElementById("lastScannedCode").style.display="none":(document.getElementById("lastScannedCode").style.display="block",document.getElementById("lastScannedCodeValue").innerText=e)})();
-//# sourceMappingURL=main.6446668a.js.map

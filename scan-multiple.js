bf69edfcbe9f43bf0d9106dda32a2b1a0b8c76b0
fernandosg <fsg_tmp@hotmail.com>
2025-01-13 14:04:35 -0600

    Updates

--- scan-multiple.js
+++ scan-multiple.js
@@ -2,66 +2,71 @@
  * Import STRICH SDK via ES6 import clause. It is recommended to pin a specific version, especially for
  * business-critical applications.
  */
-import {StrichSDK, BarcodeReader} from "https://cdn.jsdelivr.net/npm/@pixelverse/strichjs-sdk@1.6.0";
+import {
+  StrichSDK,
+  BarcodeReader
+} from 'https://cdn.jsdelivr.net/npm/@pixelverse/strichjs-sdk@1.6.0'
 
-let theBarcodeReader = null;
+let theBarcodeReader = null
 
 // data and hooks for popup dialog
-let numScannedCodes = 0;
-let scannedCodes = ['', '', '', ''];
+let numScannedCodes = 0
+let scannedCodes = ['', '', '', '']
 
 const finishButton = document.getElementById('finishScanning')
 finishButton.onclick = async () => {
-    theBarcodeReader.destroy();
-    window.location.replace('/');
+  theBarcodeReader.destroy()
+  window.location.replace('/')
 }
 
 /**
  * Scan 4 distinct barcodes and then stop.
  */
 function handleCodeDetection(codeDetection) {
-    if (numScannedCodes < 4 && scannedCodes.indexOf(codeDetection.data) === -1) {
-        scannedCodes[numScannedCodes] = codeDetection.data;
-        sessionStorage.setItem('scannedCode', codeDetection.data);
-        document.getElementById('scannedCode' + numScannedCodes).innerText = codeDetection.data;
-        numScannedCodes++;
+  if (numScannedCodes < 4 && scannedCodes.indexOf(codeDetection.data) === -1) {
+    scannedCodes[numScannedCodes] = codeDetection.data
+    sessionStorage.setItem('scannedCode', codeDetection.data)
+    document.getElementById('scannedCode' + numScannedCodes).innerText =
+      codeDetection.data
+    numScannedCodes++
 
-        // stop barcode detection when we have all the codes we need
-        if (numScannedCodes === 4) {
-            theBarcodeReader.stop();
-            finishButton.disabled = false;
-        }
+    // stop barcode detection when we have all the codes we need
+    if (numScannedCodes === 4) {
+      theBarcodeReader.stop()
+      finishButton.disabled = false
     }
+  }
 }
 
 /**
  * Initialize STRICH BarcodeReader and start scanning.
  */
 async function initializeBarcodeReader() {
-
-    // see https://docs.strich.io/reference/interfaces/Configuration.html for all available options
-    let configuration = {
-        selector: '.barcode-scanner',
-        frameSource: {
-            resolution: 'full-hd'
-        },
-        engine: {
-            symbologies: ['code128', 'qr'],
-            duplicateInterval: 1500
-        }
-    };
-
-    // initialize BarcodeReader and start scanning barcodes
-    try {
-        const barcodeReader = await new BarcodeReader(configuration).initialize();
-        barcodeReader.detected = (detections) => {
-            handleCodeDetection(detections[0]);
-        };
-        theBarcodeReader = barcodeReader;
-        await theBarcodeReader.start();
-    } catch (err) {
-        window.alert(`Failed to initialize BarcodeReader: ${err.message} ${err.detailMessage}`);
+  // see https://docs.strich.io/reference/interfaces/Configuration.html for all available options
+  let configuration = {
+    selector: '.barcode-scanner',
+    frameSource: {
+      resolution: 'full-hd'
+    },
+    engine: {
+      symbologies: ['code128', 'qr'],
+      duplicateInterval: 1500
     }
+  }
+
+  // initialize BarcodeReader and start scanning barcodes
+  try {
+    const barcodeReader = await new BarcodeReader(configuration).initialize()
+    barcodeReader.detected = (detections) => {
+      handleCodeDetection(detections[0])
+    }
+    theBarcodeReader = barcodeReader
+    await theBarcodeReader.start()
+  } catch (err) {
+    window.alert(
+      `Failed to initialize BarcodeReader: ${err.message} ${err.detailMessage}`
+    )
+  }
 }
 
 /**
@@ -71,18 +76,20 @@ async function initializeBarcodeReader() {
  * This is just for illustration, in a production app you would not be using window.alert().
  */
 function handleInitializationError(err) {
-    let message = err.message;
-    if (err.detailMessage) { // some errors contain additional details in the detailMessage property
-        message += '\n' + err.detailMessage;
-    }
-    window.alert(`Initialization failed: ${message}`);
+  let message = err.message
+  if (err.detailMessage) {
+    // some errors contain additional details in the detailMessage property
+    message += '\n' + err.detailMessage
+  }
+  window.alert(`Initialization failed: ${message}`)
 }
 
 // Initialize STRICH SDK and BarcodeReader
-const licenseKey = '<your license key>';
+const licenseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyMmI3ZGE1ZC0zY2I2LTQ1NDgtYTIwZi1kNzkyNmNlNDNiZjciLCJpc3MiOiJzdHJpY2guaW8iLCJhdWQiOlsiaHR0cHM6Ly9yYWlscy1wcm9kdWN0aW9uLTBmZjQudXAucmFpbHdheS5hcHAiLCJodHRwczovL2Zlcm5hbmRvc2cuZ2l0aHViLmlvL2JhcmNvZGVzaWktbGVjdG9yIl0sImlhdCI6MTczNjc5MTg1OCwibmJmIjoxNzM2NzkxODU4LCJjYXBhYmlsaXRpZXMiOnt9LCJ2ZXJzaW9uIjoxfQ.zW_RQOVsv02Tg6ijmFvltneV5mhs7DeofoifHK5swFg'
 try {
-    await StrichSDK.initialize(licenseKey);
-    await initializeBarcodeReader();
+  await StrichSDK.initialize(licenseKey)
+  await initializeBarcodeReader()
 } catch (err) {
-    handleInitializationError(err);
+  handleInitializationError(err)
 }
